e4a88a7fb5465e9d4ccc3725c8574a2b07e3df70
Hakes2000Asome <rickyradantrlr@icloud.com>
2025-10-14 14:55:52 -0700

    changed wheel angle

--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -9,13 +9,9 @@ float xcurrent = 16.86-7.5; //inches
 float previous = 0; //centidegrees
 int wheel_diameter = 2.625;
 float wheel_ratio = 1.389*(wheel_diameter*std::numbers::pi)/36000;
-float angle_wheel_radius = 2.663;
 
 float wheel_angle(){
-    float arc_length =  (rotation2_sensor.get_position()) * wheel_ratio;
-    float angle = arc_length / angle_wheel_radius;
-    delay(10);
-    return angle;
+    
 }
 float accurate_angle(){
     return reduce_0_to_360(imu_sensor.get_heading()+90);
