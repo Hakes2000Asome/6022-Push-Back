19e032775a3622487161d2b65d283e08e57ea4e7
Kaursome10 <simrankaursome@gmail.com>
2025-10-14 15:01:11 -0700

    Update Drive moveBack() function.cpp

--- src/Drive.cpp
+++ src/Drive.cpp
@@ -95,3 +95,38 @@ int turn(int angle){
         return 1;
     }
 }
+bool moveBack(int x_cord, int y_cord){
+    int theta = atan2((x_cord - current_x_pose()), (y_cord- current_y_pose()))*360/(2*std::numbers::pi); //calculating angle
+
+    if ((sqrt( pow((x_cord - current_x_pose()), 2) + pow((y_cord - current_y_pose()), 2) ) > drive_threshold ) ){
+        
+        float delta_distance = (sqrt( pow((x_cord - current_x_pose()), 2) + pow((y_cord - current_y_pose()), 2) )); //distance formula
+        float delta_angle = reduce_negative_180_to_180(theta-accurate_angle()); //how much the bot needs to turn
+
+        float power = drive_voltage*pow((delta_distance/drive_slowdown), drive_power)+drive_minimum*abs(pow(delta_distance/drive_threshold,(1/drive_power)))*delta_distance/abs(delta_distance);
+        float power_turn = turn_voltage*pow((delta_angle/turn_slowdown), turn_power)+turn_minimum*abs(pow(delta_angle/turning_threshold,(1/turn_power)))*delta_angle/abs(delta_angle);
+        
+        //to keep power within range of -127 to 127
+        if (power >= 127){
+            power = 127;
+        }
+        if (power <= -127){
+            power = -127;
+        }
+        if (power_turn >= 127){
+            power_turn = 127;
+        }
+        if (power_turn <= -127){
+            power_turn = -127;
+        }
+        motor_group_left.move(-power+turn_power); //**********IS THIS SUPPOSED TO BE (power+POWER_TURN)???
+        motor_group_right.move(-power-turn_power);
+        return 0;
+    }
+
+    motor_group_left.move(0);
+    motor_group_right.move(0);
+    return 1;
+
+}
+
