3ea4bf85abb572bfa303ac524f1c60cce786a76d
Kaursome10 <simrankaursome@gmail.com>
2025-11-13 16:50:01 -0800

    updated port numbers

--- src/setup.cpp
+++ src/setup.cpp
@@ -9,29 +9,31 @@ Motor motor_back_right (14);
 MotorGroup motor_group_right ({14, 15, 16}, pros::v5::MotorGears::blue);
 
 //left motors
-Motor motor_front_left (1);
+Motor motor_front_left (11);
 Motor motor_middle_left (12);
 Motor motor_back_left (13);
-MotorGroup motor_group_left({-1, -12, -13}, pros::v5::MotorGears::blue);
+MotorGroup motor_group_left({-11, -12, -13}, pros::v5::MotorGears::blue);
  
 //inertial sensor
-Imu imu_sensor(21);
+// Imu imu_sensor(21);
 
 //controller
 Controller master (E_CONTROLLER_MASTER);
 
 //rotation sensor
-Rotation r_rotation_sensor(2);
-Rotation l_rotation_sensor(3);
-Rotation s_rotation_sensor(5);
+// Rotation r_rotation_sensor(2);
+// Rotation l_rotation_sensor(3);
+// Rotation s_rotation_sensor(5);
 
 //intakae motor
-Motor intake(9);
-Motor storage(20);
-Motor top(10);
+// Motor intake(9);
+// Motor storage(20);
+// Motor top(10);
+Motor conveyor(1);
+Motor railGun(2);
 
 //air
-ADIDigitalOut piston (8);
+ADIDigitalOut piston ('G');
 
 //color sensor
-Optical optical_sensor (4);
+// Optical optical_sensor (4);
